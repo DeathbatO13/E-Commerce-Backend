db42177e0183535e09877d1ec312da1c38f9f979
DeathbatO13 <danielalejo99@gmail.com>
2026-02-21 17:16:32 -0500

    Added cart service annotations left

--- cart-service/src/main/java/com/e_commerce/cart_service/adapter/out/persistence/repository/CartJpaRepository.java
+++ cart-service/src/main/java/com/e_commerce/cart_service/adapter/out/persistence/repository/CartJpaRepository.java
@@ -2,10 +2,12 @@ package com.e_commerce.cart_service.adapter.out.persistence.repository;
 
 import com.e_commerce.cart_service.adapter.out.persistence.entity.CartJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface CartJpaRepository extends JpaRepository<CartJpaEntity, UUID>{
 
     Optional<CartJpaEntity> findByUserId(UUID userId);
