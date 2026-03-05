cf5f69f1200b953d556a070becfb06dc34466354
DeathbatO13 <danielalejo99@gmail.com>
2026-03-04 20:21:55 -0500

    Add order service basic domain repository

---
+++ order-service/src/main/java/com/e_commerce/order_service/domain/ports/out/OrderRepositoryPort.java
@@ -0,0 +1,16 @@
+package com.e_commerce.order_service.domain.ports.out;
+
+import com.e_commerce.order_service.domain.model.Order;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface OrderRepositoryPort {
+
+    Order save(Order order);
+
+    Optional<Order> findById(UUID id);
+
+    List<Order> findAll();
+}
