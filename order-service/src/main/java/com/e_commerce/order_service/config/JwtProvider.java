35997723ee922a4fe3e9bf400612aabacf9b1796
Daniel Torres <danielalejo99@gmail.com>
2026-07-04 18:28:18 -0500

    Add Jwt Provider in order service

--- order-service/src/main/java/com/e_commerce/order_service/config/JwtProvider.java
+++ order-service/src/main/java/com/e_commerce/order_service/config/JwtProvider.java
@@ -1,4 +1,49 @@
 package com.e_commerce.order_service.config;
 
-public class JwtProvider {
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtProvider{
+
+    //@Value()
+    private String secret;
+
+
+    public Claims getClaims(){
+        return Jwts.parserBuilder()
+                .setSigningKey(secret.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            // Si el token expiró, la firma es incorrecta o está mal formado, lanzará una excepción.
+            this.getClaims(token);
+            return true;
+        } catch (io.jsonwebtoken.ExpiredJwtException e) {
+            System.err.println("El token JWT ha expirado: " + e.getMessage());
+        } catch (io.jsonwebtoken.security.SignatureException e) {
+            System.err.println("Firma del JWT inválida: " + e.getMessage());
+        } catch (io.jsonwebtoken.MalformedJwtException e) {
+            System.err.println("Token JWT mal formado: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Error al validar el token JWT: " + e.getMessage());
+        }
+        return false;
+    }
+
+
+    public long extractUserId(String token){
+        return getClaims(token).get("userId", Long.class);
+    }
+
+    public String extractEmail(String token){
+        return getClaims(token).getSubject();
+    }
 }
